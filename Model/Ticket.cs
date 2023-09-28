5703b35df26eab039d83abddf08aae9158687689
Abdelrahman a Ghonim <695857@student.inholland.nl>
2023-09-28 17:20:42 +0200

    Dao and Model layer done

---
+++ Model/Ticket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+namespace Model
+{
+    public class Ticket
+    {
+        [BsonElement("_id")]
+        public string TicketId { get; set; }
+
+        [BsonElement("Description")]
+        public string Description { get; set; }
+
+
+        [BsonElement("Subject")]
+        public string subject { get; set; }
+
+        [BsonElement("DataAdded")]
+        public DateTime DateAdded { get; set; }
+
+        [BsonElement("DeadLine")]
+        public DateTime Deadline { get; set; }
+
+        [BsonElement("LastUpdated")]
+        public DateTime LastUpdated { get; set; }
+
+        [BsonElement("Priority")]
+        public Priority Priority { get; set; }
+
+        [BsonElement("Status")]
+        public Status Status { get; set; }
+
+
+    }
+}
