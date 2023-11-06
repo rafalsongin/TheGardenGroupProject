b6a6bd8c536345f758d347b5e6255f1476d5987f
Rafal Songin <rafal.songin@gmail.com>
2023-11-06 21:31:33 +0100

    Pie chart is being displayed

--- UI/DashboardUI.xaml.cs
+++ UI/DashboardUI.xaml.cs
@@ -3,8 +3,11 @@ using Model;
 using Service;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using System.Security.Principal;
 using System.Windows;
+using DAL;
 using LiveCharts;
+using LiveCharts.Charts;
 using LiveCharts.Wpf;
 using UI;
 
@@ -30,7 +33,7 @@ namespace TheGardenGroupProject
             DataContext = this;
             
             // Display PieChart
-            //DisplayPieChart();
+            DisplayPieChart();
         }
 
         /*public DashboardUI()
@@ -47,37 +50,68 @@ namespace TheGardenGroupProject
         
         private void DisplayPieChart()
         {
-            TicketStatusData = new SeriesCollection
+            var ticketDao = new TicketDao();
+            var tickets = ticketDao.GetAllTickets();
+
+            List<Ticket> openedTickets = new List<Ticket>();
+            List<Ticket> resolvedTickets = new List<Ticket>();
+            List<Ticket> closedTickets = new List<Ticket>();
+            
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Status == Status.Opened)
+                {
+                    openedTickets.Add(ticket);
+                }
+                else if (ticket.Status == Status.Resolved)
+                {
+                    resolvedTickets.Add(ticket);
+                }
+                else if (ticket.Status == Status.Closed)
+                {
+                    closedTickets.Add(ticket);
+                }
+            }
+            
+            SeriesCollection pieSeries = new SeriesCollection
             {
                 new PieSeries
                 {
-                    Title = "Open",
-                    Values = new ChartValues<double> { 5 },
+                    Title = "Opened tickets",
+                    Values = new ChartValues<double> { openedTickets.Count },
                     DataLabels = true,
-                    LabelPoint = TicketStatusLabelPoint,
-                    Fill = System.Windows.Media.Brushes.Green
+                    FontSize = 20
                 },
                 new PieSeries
                 {
-                    Title = "Closed",
-                    Values = new ChartValues<double> { 3 },
+                    Title = "Resolved tickets",
+                    Values = new ChartValues<double> { resolvedTickets.Count },
                     DataLabels = true,
-                    LabelPoint = TicketStatusLabelPoint,
-                    Fill = System.Windows.Media.Brushes.Red
+                    FontSize = 20
                 },
                 new PieSeries
                 {
-                    Title = "In Progress",
-                    Values = new ChartValues<double> { 2 },
+                    Title = "Closed tickets",
+                    Values = new ChartValues<double> { closedTickets.Count },
                     DataLabels = true,
-                    LabelPoint = TicketStatusLabelPoint,
-                    Fill = System.Windows.Media.Brushes.Yellow
+                    FontSize = 20
                 }
             };
+
+            PieChart pieChart = new PieChart
+            {
+                Series = pieSeries,
+                LegendLocation = LegendLocation.Right,
+                Width = 400,
+                Height = 400,
+                Margin = new Thickness(10, 10, 10, 10),
+                Hoverable = true,
+                DisableAnimations = true,
+                DataTooltip = null
+            };
             
-            TicketStatusLabelPoint = chartPoint => $"{chartPoint.Y} ({chartPoint.Participation})";
+            ChartContainer.Children.Add(pieChart);
             
-            DataContext = this;
         }
     }
 }
