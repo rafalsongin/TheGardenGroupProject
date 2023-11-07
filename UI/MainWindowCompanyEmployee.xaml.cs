c5fb59f1bc1b2a6a897c2e782997318378b1aac5
Rafal Songin <rafal.songin@gmail.com>
2023-11-07 18:35:22 +0100

    page switching service

---
+++ UI/MainWindowCompanyEmployee.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TheGardenGroupProject;
+
+public partial class MainWindowCompanyEmployee : Window
+{
+    public MainWindowCompanyEmployee()
+    {
+        InitializeComponent();
+    }
+
+    private void DashboardButton_Click(object sender, RoutedEventArgs e)
+    {
+        // ContentPage.Content = new Page1();
+        // ContentPage.NavigationService.Navigate(new Page1());
+        ContentPage.Content = new Page1();
+        // ContentPage.NavigationService.Source = null;
+        // ContentPage.NavigationService.RemoveBackEntry();
+    }
+
+    private void ViewUsersButton_Click(object sender, RoutedEventArgs e)
+    {
+        // ContentPage.Content = new Page2();
+        // ContentPage.NavigationService.Navigate(new Page2());
+        ContentPage.Content = new Page2();
+        // ContentPage.NavigationService.Source = null;
+        // ContentPage.NavigationService.RemoveBackEntry();
+    }
+}
