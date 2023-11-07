6b8d0754a5acfcfa6d64fec13a87db726ca6ea6a
Rafal Songin <rafal.songin@gmail.com>
2023-11-07 18:44:53 +0100

    Testing of page switching service is successful

--- UI/MainWindowCompanyEmployee.xaml.cs
+++ UI/MainWindowCompanyEmployee.xaml.cs
@@ -12,19 +12,11 @@ public partial class MainWindowCompanyEmployee : Window
 
     private void DashboardButton_Click(object sender, RoutedEventArgs e)
     {
-        // ContentPage.Content = new Page1();
-        // ContentPage.NavigationService.Navigate(new Page1());
         ContentPage.Content = new Page1();
-        // ContentPage.NavigationService.Source = null;
-        // ContentPage.NavigationService.RemoveBackEntry();
     }
 
     private void ViewUsersButton_Click(object sender, RoutedEventArgs e)
     {
-        // ContentPage.Content = new Page2();
-        // ContentPage.NavigationService.Navigate(new Page2());
         ContentPage.Content = new Page2();
-        // ContentPage.NavigationService.Source = null;
-        // ContentPage.NavigationService.RemoveBackEntry();
     }
 }
