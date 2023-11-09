ca7123de34d6232c296f10a26b2255779f8d9b70
KimvanSchagen <95349001+KimvanSchagen@users.noreply.github.com>
2023-11-09 22:25:56 +0100

    fix errors

--- Service/TicketService.cs
+++ Service/TicketService.cs
@@ -49,45 +49,9 @@ namespace Service
         {
             return ticketDao.GetClosedTickets();
         }
-
-        public long GetTicketCountForUser(string userEmail)
-        {
-            return ticketDao.GetTicketCountForUser(userEmail);
-
         public List<Ticket> GetAllTickets()
         {
             return ticketDao.GetAllTickets();
-        }
-
-        //kim
-        public List<Ticket> GetAllTicketsFromUser(User user)
-        {
-            return ticketDao.GetAllTicketsFromUser(user);
-        }
-
-        public List<Ticket> GetOpenTicketsFromUser(User user)
-        {
-            return ticketDao.GetOpenTicketsFromUser(user);
-        }
-
-        public List<Ticket> GetClosedTicketsFromUser(User user)
-        {
-            return ticketDao.GetClosedTicketsFromUser(user);
-        }
-
-        public List<Ticket> GetAllTickets1()
-        {
-            return ticketDao.GetAllTickets();
-        }
-
-        public List<Ticket> GetAllOpenTickets()
-        {
-            return ticketDao.GetAllOpenTickets();
-        }
-
-        public List<Ticket> GetAllClosedTickets()
-        {
-            return ticketDao.GetAllClosedTickets();
         }
     }
 }
